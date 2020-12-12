63a24cb104420016d63d9a618572af7a4c04ace1
secbro <xinyoulingxi2008@126.com>
2020-12-12 19:32:47 +0800

    相册功能

--- learn-javafx-003-photos/src/main/java/sample/Main.java
+++ learn-javafx-003-photos/src/main/java/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -9,29 +10,53 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	String[] imagesUrls = {
+			"01.png", "02.jpg"
+	};
+	Image[] images = new Image[2];
+	ImageView imageView = new ImageView();
+	/**
+	 * 当前显示的图片
+	 */
+	int currentIndex = 0;
+
 	@Override
 	public void start(Stage primaryStage) {
 
 		BorderPane root = new BorderPane();
+		// 初始化第一个图片
+		imageView.setImage(new Image(imagesUrls[0]));
 
-		// 加载网络图片
-//		Image image = new Image("https://www.choupangxia.com/wp-content/uploads/2019/07/weixin.jpg");
-		// 加载本地图片
-//		Image image = new Image("file:/Users/zzs/my/images/QQ20180302-0.png");
-		// 加载classpath或资源文件文件
-		Image image = new Image("QQ20180302-0.png");
-		ImageView imageView = new ImageView();
-		imageView.setImage(image);
+		Button button = new Button("下一张");
+		button.setOnAction(event -> {
+			showNext();
+		});
+
+		// 初始化images内容
+		for (int i = 0; i < images.length; i++) {
+			images[i] = new Image(imagesUrls[i]);
+		}
+
+		imageView.setPreserveRatio(true);
+		imageView.setFitWidth(400);
+		imageView.setFitHeight(400);
+
+		root.setTop(button);
 		root.setCenter(imageView);
 
-		double with = image.getWidth();
-		double height = image.getHeight();
-
-		Scene scene = new Scene(root, with, height);
+		Scene scene = new Scene(root, 400, 400);
 		scene.getStylesheets().add(getClass().getResource("sample.fxml").toExternalForm());
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	public void showNext() {
+		currentIndex++;
+		if (currentIndex >= 2) {
+			currentIndex = 0;
+		}
+		imageView.setImage(images[currentIndex]);
 	}
 
 	public static void main(String[] args) {
